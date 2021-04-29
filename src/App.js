4168a934adc1258329b17a3c05ffd7e876051da9
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-29 18:23:27 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    username: "",
+    email: "",
+  };
+
+  handleChange = (e) => {
+    console.log(e.target.type);
+    console.log(e.target.name);
+
+    const username = e.target.value;
+
+    this.setState({
+      username,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor="username">
+            Twoję imię:
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="email">
+            Twoję adres email:
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </label>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
