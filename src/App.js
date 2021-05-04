6ef3a17ae52906f230660b8404e8093d56e627be
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-05-04 19:41:51 +0200

    validation

--- src/App.js
+++ src/App.js
@@ -45,30 +45,69 @@ class App extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
 
-    if (true) {
-      this.setState({
-        username: "",
-        email: "",
-        password: "",
-        accept: false,
+    const validation = this.formValidation();
+    console.log(validation);
 
-        errors: {
-          username: false,
-          email: false,
-          password: false,
-          accept: false,
-        },
-      });
-    } else {
-      this.setState({
-        errors: {
-          username: false,
-          email: false,
-          password: false,
-          accept: false,
-        },
-      });
+    // if (true) {
+    //   this.setState({
+    //     username: "",
+    //     email: "",
+    //     password: "",
+    //     accept: false,
+
+    //     errors: {
+    //       username: false,
+    //       email: false,
+    //       password: false,
+    //       accept: false,
+    //     },
+    //   });
+    // } else {
+    //   this.setState({
+    //     errors: {
+    //       username: false,
+    //       email: false,
+    //       password: false,
+    //       accept: false,
+    //     },
+    //   });
+    // }
+  };
+
+  formValidation = () => {
+    let username = false;
+    let email = false;
+    let password = false;
+    let accept = false;
+    let correct = false;
+
+    if (this.state.username.length > 10 && this.state.username.indexOf(" ") === -1) {
+      username = true;
     }
+
+    if (this.state.email.indexOf("@") !== -1) {
+      email = true;
+    }
+
+    if (this.state.password.length === 8) {
+      password = true;
+    }
+
+    if (this.state.accept) {
+      accept = true;
+    }
+
+    if (username && email && password && accept) {
+      correct = true;
+    }
+
+    return {
+      username,
+      email,
+      password,
+      accept,
+      correct,
+    };
   };
 
   render() {
